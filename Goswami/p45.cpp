c66db57a1e2cf94c628753cddb9877cbe93e9a55
Pankaj Jha <jha3@llnl.gov>
2025-04-09 23:33:27 -0700

    Add comments about operator precedence

--- Goswami/p45.cpp
+++ Goswami/p45.cpp
@@ -34,10 +34,11 @@ main()
   cl a(1,2,3),b(3,5,2),c(0,0,0);
   cout<<a<<" "<<b<<endl;
   //cout.setf(ios::showpos);
-  cout<<a+100;
+  cout<<a+100; // Operator is called on 'a' and 100 added
   cout<<endl;
-  cout<<(a+b)<<endl;//this gives 315 as o/p
-  // The output from below expression is not very clear 
+  
   c=a+b; // integer operation takes precence over class operation
-  cout<<c<<endl;
+  cout<<c<<endl; // calls teh ostream, hence displays contents of a+b
+
+  cout<<(a+b)<<endl;//Operator is aclledon the result of (a+b)
 }
