57f8439d9b54a6c83932ebaa7097e1a586387436
Pankaj Jha <jha3@llnl.gov>
2025-04-09 20:33:10 -0700

    Update teh code for pass by value and reference

--- Goswami/p15.cpp
+++ Goswami/p15.cpp
@@ -11,21 +11,36 @@ public:
   void out_i(){cout<<"\ni: "<<i;}
 };
 
-void f(obj a);
+void f1(obj a); // Pass by value
+void f2(obj &a); // Pass by reference
 
 main()
 {
   obj b;
   b.set_i(10);
-  f(b);
+
+  f1(b);
+  cout<<"\nIn main i: ";
+  b.out_i();
+  cout<<"\n";
+
+  f2(b);
   cout<<"\nIn main i: ";
   b.out_i();
   cout<<"\n";
 }
-
-void f(obj a)  // only a copy of the object a is passed
+void f1(obj a) // Pass by value, only a copy of the object a is passed
 {
-  cout<<"\nIn func f, i :";
+  cout<<"\nIn func f1, i :";
+  a.out_i();
+  a.set_i(100);
+  cout<<"\nAfter changing in func,i :";
+  a.out_i();
+
+}
+void f2(obj &a)  // Pass by reference
+{
+  cout<<"\nIn func f2, i :";
   a.out_i();
   a.set_i(100);
   cout<<"\nAfter changing in func,i :";
